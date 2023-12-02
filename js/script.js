62d30f680b64ce1c640b0fb4b1b7a8196d48050d
yushin0319 <yushin.fudo1986@gmail.com>
2023-12-02 17:31:31 +0900

    example change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-document.getElementById("clickMe").addEventListener("click", function () {
-  alert("Button clicked!");
+window.addEventListener("DOMContentLoaded", () => {
+  const mainImage = document.querySelector("header img");
+  if (mainImage) {
+    mainImage.style.opacity = 1; // 不透明度を1に設定
+  }
 });
